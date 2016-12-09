ba3939cabfe11e0369f081be6849b8b8dc70dc5e
Emmanuel <manolodigial@gmail.com>
2016-12-09 18:13:15 +0000

    Models Productos fixed

--- Inventarios/models.py
+++ Inventarios/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Productos(models.Model):
     name = models.CharField(max_length=60, blank=False)
     description = models.CharField(max_length=100, blank=True)
-    unitprice = models.DecimalField()
+    unitprice = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField()
     category = models.CharField(max_length=30, blank=False)
-    images = models.FileField()
+    images = models.FileField(upload_to="uploads/")
     vendedor = models.ForeignKey(Vendedor, related_name="producto-vendedor")
+    
