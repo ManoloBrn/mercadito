6a0cb3e5d5833712e6d3ab7ee0e3623bc84bc908
Emmanuel <manolodigial@gmail.com>
2016-12-09 15:47:56 +0000

    Productos modelo

--- Inventarios/models.py
+++ Inventarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Productos(models.Model):
+    name = models.CharField(max_length=60, blank=False)
+    description = models.CharField(max_length=100, blank=True)
+    unitprice = models.DecimalField()
+    quantity = models.IntegerField()
+    category = models.CharField(max_length=30, blank=False)
+    images = models.FileField()
+    vendedor = models.ForeignKey(Vendedor, related_name="producto-vendedor")
