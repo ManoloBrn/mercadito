eabf24a2349308525531dc2a9d097f26e9016d3f
Emmanuel <manolodigial@gmail.com>
2016-12-14 13:23:41 +0000

    Productos por vendedor

--- Inventarios/serializers.py
+++ Inventarios/serializers.py
@@ -5,3 +5,8 @@ class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = "__all__"
+
+class ProductoVentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ["name", "description", "unitprice", "quantity"]
